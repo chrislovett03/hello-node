d57c90790fb0c23af9bb9a5704474d1eb3894434
Ubuntu <ubuntu@ip-172-31-8-34.ec2.internal>
2017-09-10 23:18:56 +0000

    File change.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function(req, res) {
-  res.send('Hello World!')
+  res.send('Hello Big World!')
 })
 
 // Launch listenting server on port 8081
