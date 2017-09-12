6794014202010a63d33162af6141893f56ef71dd
Christine Lovett <christinelovett@Dockers-MacBook-Pro.local>
2017-09-12 11:48:34 -0400

    Updated content for app.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.static(__dirname + '/public'))
+
 
 //Define request response in root URL (/)
 app.get('/', function(req, res) {
-  res.send('Watch out for Irma!')
-})
+    res.render('home');
+});
+
+
+// app.get('/', function(req, res) {
+//   res.send('Watch out for Irma!')
+// })
 
 // Launch listenting server on port 8081
 app.listen(8081, function() {
